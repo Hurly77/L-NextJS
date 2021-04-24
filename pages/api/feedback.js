05a3cde4c0c78135824c98d26b487075f73c2f9f
Cameron Leverett <camrbo@gmail.com>
2021-04-23 21:11:40 -0700

    added post request to dummy json and api route

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,42 @@
-const handler = (req, res) => {
-  res.status(200).json({message: 'This works!'})
+import fs from 'fs';
+import path from 'path';
+
+const id = () => {
+  let int = Math.random().toString();
+  let id = int.slice(2);
+  return parseInt(id);
+};
+
+const buildFeedbackPath = () => {
+  const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+};
+
+const extractFeedback = (filePath) => {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
 }
 
-export default handler
+const handler = (req, res) => {
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.feedback;
+
+    const newFeedback = {
+      id: id(),
+      email: email,
+      text: feedbackText,
+    };
+
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath)
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success!', feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath()
+    const data = extractFeedback(filePath)
+    res.status(200).json({ message: 'This works!' });
+  }
+};
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,49 @@
-import {useRef} from 'react'
+import { useRef } from 'react';
 
 function HomePage () {
-  const emailInputRef = useRef()
-  const feedbackInputRef = useRef()
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
-    const email = emailInputRef.current.value
-    const feedback = feedbackInputRef.current.value
+    const inputEmail = emailInputRef.current.value;
+    const inputFeedback = feedbackInputRef.current.value;
+    console.log(inputEmail, inputFeedback)
 
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify({ email: inputEmail, feedback: inputFeedback }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const loadFeedbackHandler = () => {
+    fetch('/api/feedback')
   }
-  
-	return (
-		<div>
-			<h1>The Home Page</h1>
-			<form>
-				<div>
-				<label htmlFor='email'>Your Email address</label>
-        <input type="email" id="email" ref={emailInputRef} />
+
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your Email address</label>
+          <input type='email' id='email' ref={emailInputRef} />
         </div>
         <div>
-          <label htmlFor="feedback">Your FeedBack</label>
-          <textarea id="feedback" rows="4" ref={feedbackInputRef}></textarea>
+          <label htmlFor='feedback'>Your FeedBack</label>
+          <textarea id='feedback' rows='4' ref={feedbackInputRef} />
         </div>
-			</form>
-		</div>
-	);
+        <button type="submit">Submit</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+    </div>
+  );
 }
 
 export default HomePage;
